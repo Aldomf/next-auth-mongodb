dda06bf0c610e32d5addaf3aaeacd2cc207c69b7
Aldo Miralles <alditomiralles3@gmail.com>
2023-08-11 15:33:57 +0200

    dashboard/profile

--- src/app/dashboard/profile/page.tsx
+++ src/app/dashboard/profile/page.tsx
@@ -9,21 +9,16 @@ function ProfilePage() {
 
   return (
     <div className="flex justify-center items-center">
-      <div className="flex flex-col items-center">
+      <div className="flex flex-col items-center bg-white rounded-md p-8 shadow-md mt-5">
         <h1 className="text-5xl mt-4 mb-4">Profile</h1>
-        <pre className="bg-gray-100 p-4 rounded-md">
-          {JSON.stringify(
-            {
-              session,
-              status,
-              email: session?.user?.email,
-            },
-            null,
-            2
-          )}
-        </pre>
+        <div className="text-center">
+          <h2 className="font-bold">{`ID: ${session?.user?._id}`}</h2>
+          <p className="text-gray-500">{session?.user?.fullname}</p>
+          <p className="text-gray-500">{session?.user?.email}</p>
+          <p className="text-green-500">{status}</p>
+        </div>
         <button
-          className="max-w-7xl bg-red-500 text-white p-4 rounded-md mt-4"
+          className="bg-red-500 text-white p-4 rounded-md mt-4"
           onClick={() => {
             signOut();
           }}
@@ -36,4 +31,5 @@ function ProfilePage() {
 }
 
 export default ProfilePage;
+
 
